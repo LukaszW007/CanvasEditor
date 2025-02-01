95e6f44280f9ca32f76c50c08eea85d49300acec
Lukasz <lukasz.wiszniewski007@gmail.com>
2025-02-01 19:34:14 +0100

    Added configuration of Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,46 @@
+/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins", ...fontFamily.sans],
+			},
+			colors: {
+				transparent: "transparent",
+				primary: {
+					100: "#7209B7",
+					50: "#7209B750",
+				},
+				black: {
+					100: "#353535",
+					75: "#676767",
+					50: "#9b9b9b",
+					25: "#cdcdcd",
+				},
+				white: {
+					100: "#fff",
+					98: "#fafafa",
+					97: "#f7f7f8",
+				},
+			},
+			fontSize: {
+				button: "15px",
+				display: "32px",
+				body: "18px",
+			},
+			fontWeight: {
+				normal: 400,
+				medium: 500,
+				semibold: 600,
+				bold: 700,
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [require("@tailwindcss/postcss7-compat")],
+};
